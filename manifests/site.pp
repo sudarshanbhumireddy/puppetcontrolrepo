587b0c290683fdc082c150aaaa593be7e1d0182c
sudarshanbhumireddy <74098301+sudarshanbhumireddy@users.noreply.github.com>
2025-03-18 08:06:13 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,6 +26,10 @@ File { backup => false }
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
 Notify{ 'hello from server2': }
+package { 'java':
+    ensure => installed
+    }
+    
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
