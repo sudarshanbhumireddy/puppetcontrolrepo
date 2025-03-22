5b946b9eebc8fbf9041bf952c7eab0883ece16ed
sudarshanbhumireddy <74098301+sudarshanbhumireddy@users.noreply.github.com>
2025-03-22 16:04:12 +0530

    Modified iis

--- modules/windows_webserver/manifests/init.pp
+++ modules/windows_webserver/manifests/init.pp
@@ -1,20 +1,26 @@
-class windows_webserver{
-iis_application_pool { 'minimal_site_app_pool':
-  ensure                  => 'present',
-  state                   => 'started',
-  managed_pipeline_mode   => 'Integrated',
-  managed_runtime_version => 'v4.0',
-} ->
+$iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+iis_feature { $iis_features:
+  ensure => 'present',
+}
+
+# Delete the default website to prevent a port binding conflict.
+iis_site {'Default Web Site':
+  ensure  => absent,
+  require => Iis_feature['Web-WebServer'],
+}
 
 iis_site { 'minimal':
   ensure          => 'started',
   physicalpath    => 'c:\\inetpub\\minimal',
-  applicationpool => 'minimal_site_app_pool',
-  require         => File['minimal'],
+  applicationpool => 'DefaultAppPool',
+  require         => [
+    File['minimal'],
+    Iis_site['Default Web Site']
+  ],
 }
 
 file { 'minimal':
   ensure => 'directory',
   path   => 'c:\\inetpub\\minimal',
-}
 }
