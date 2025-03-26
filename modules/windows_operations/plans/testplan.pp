0a6251dc4db5015f7404ca59d01e23e1f9cafb20
sudarshanbhumireddy <74098301+sudarshanbhumireddy@users.noreply.github.com>
2025-03-26 08:45:29 +0530

    Added plan

--- modules/windows_operations/plans/testplan.pp
+++ modules/windows_operations/plans/testplan.pp
@@ -1,4 +1,6 @@
 plan windows_operations::referencing_the_primary_server(){
   # Note that if you tried to use `localhost` instead of `my-primary-server` this plan would fail!
-  run_command(‘whoami’, ‘desktop-4mtampd.localdomain’)
+  run_task('get_processinfo', 
+    name   => 'explorer'
+  )
 }
