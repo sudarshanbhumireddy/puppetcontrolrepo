3e13e975e172393aa0c383a2f9933a7acc38ce49
sudarshanbhumireddy <74098301+sudarshanbhumireddy@users.noreply.github.com>
2025-03-26 08:48:37 +0530

    Modified plan

--- modules/windows_operations/plans/test_process.pp
+++ modules/windows_operations/plans/test_process.pp
@@ -1,4 +1,4 @@
-plan windows_operations::referencing_the_primary_server(){
+plan windows_operations:: test_process() {
   # Note that if you tried to use `localhost` instead of `my-primary-server` this plan would fail!
   run_task('get_processinfo', 
     name   => 'explorer'
