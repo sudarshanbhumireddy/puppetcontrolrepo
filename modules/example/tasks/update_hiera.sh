2795842c59707a7ea4e3327626781b35b2d90eff
sudarshanbhumireddy <74098301+sudarshanbhumireddy@users.noreply.github.com>
2025-03-27 23:25:39 +0530

    Updated

--- modules/example/tasks/update_hiera.sh
+++ modules/example/tasks/update_hiera.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-# Define the standard path for the YAML file
-FILE_PATH="/etc/puppetlabs/code/environments/production/data/common.yaml"
-
-echo "Checking if file exists: $FILE_PATH"
-
 echo "All updates completed successfully!"
